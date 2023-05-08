4906c89d2f6bfeb4713df9ebc723115bc77518f1
HoDoan <doanphj5@gmail.com>
2023-05-08 17:02:07 +0700

    complete-sdk

--- protos/run.sh
+++ protos/run.sh
@@ -1,5 +1,5 @@
-flutter pub global deactivate protoc_plugin
-flutter pub global activate protoc_plugin
+# flutter pub global deactivate protoc_plugin
+# flutter pub global activate protoc_plugin
 dart pub global activate protoc_plugin
 export PATH="$PATH":"$HOME/.pub-cache/bin"
 protoc --dart_out=../lib/src/generated ./blesdk.proto
