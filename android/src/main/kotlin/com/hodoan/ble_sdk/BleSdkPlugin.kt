95f7f829381f7c25cdc98c7e4ef6174ce2917865
HoDoan <doanphj5@gmail.com>
2023-08-03 16:13:29 +0700

    delay-notification-characteristic

--- android/src/main/kotlin/com/hodoan/ble_sdk/BleSdkPlugin.kt
+++ android/src/main/kotlin/com/hodoan/ble_sdk/BleSdkPlugin.kt
@@ -214,24 +214,24 @@ class BleSdkPlugin : FlutterPlugin, MethodCallHandler, IBleClientCallBack, Activ
     private fun setNotification(call: MethodCall, result: Result) {
         val charValue = Characteristic.parseFrom(call.arguments as ByteArray)
         val isResult = bleClient.notificationCharacteristic(charValue)
-        if (isResult) {
-            logEvent.success(
-                Log.newBuilder().setCharacteristic(charValue)
-                    .setMessage("notification ${charValue.characteristicId}").build()
-            )
-        }
+        logEvent.success(
+            Log.newBuilder().setCharacteristic(charValue)
+                .setMessage("notification ${charValue.characteristicId} status bool: $isResult").build()
         result.success(isResult)
     }
 
     private fun setIndication(call: MethodCall, result: Result) {
         val charValue = Characteristic.parseFrom(call.arguments as ByteArray)
         val isResult = bleClient.indicationCharacteristic(charValue)
-        if (isResult) {
-            logEvent.success(
-                Log.newBuilder().setCharacteristic(charValue)
-                    .setMessage("indicator ${charValue.characteristicId}").build()
-            )
+        //if (isResult) {
+        //    logEvent.success(
+        //        Log.newBuilder().setCharacteristic(charValue)
+        //            .setMessage("indicator ${charValue.characteristicId}").build()
+        //    )
         }
+        logEvent.success(
+            Log.newBuilder().setCharacteristic(charValue)
+                .setMessage("indicator ${charValue.characteristicId} status bool: $isResult").build()
         result.success(isResult)
     }
 
--- lib/src/ble_sdk_custom.dart
+++ lib/src/ble_sdk_custom.dart
@@ -25,30 +25,74 @@ class BleSdk extends BleSdkNative {
         completer.complete(values);
       });
     });
+    // final listNotifications = [
+    //   value.characteristic,
+    //   for (final notify in notifications ?? <Characteristic>[]) notify,
+    // ].map(
+    //   (e) => Future(() {
+    //     if (e.properties.contains(CharacteristicProperties.NOTIFY)) {
+    //       setNotification(e);
+    //     }
+    //     if (e.properties.contains(CharacteristicProperties.INDICATE)) {
+    //       setIndication(e);
+    //     }
+    //   }),
+    // );
+    // final checkNotify = value.characteristic.properties
+    //         .contains(CharacteristicProperties.NOTIFY) ||
+    //     value.characteristic.properties
+    //         .contains(CharacteristicProperties.INDICATE);
+    // if (checkNotify) {
+    //   await Future.forEach(listNotifications, (notify) => notify);
+    //   await Future<void>.delayed(const Duration(milliseconds: 450));
+    // }
+    // final isWrite = await writeCharacteristicNoResponse(value);
+    // if (!checkNotify) {
+    //   await Future.forEach(listNotifications, (notify) => notify);
+    // }
     final listNotifications = [
       value.characteristic,
       for (final notify in notifications ?? <Characteristic>[]) notify,
-    ].map(
-      (e) => Future(() {
-        if (e.properties.contains(CharacteristicProperties.NOTIFY)) {
-          setNotification(e);
-        }
-        if (e.properties.contains(CharacteristicProperties.INDICATE)) {
-          setIndication(e);
-        }
-      }),
-    );
+    ]
+        .map(
+          (e) => Future<bool>(() {
+            if (e.properties.contains(CharacteristicProperties.NOTIFY)) {
+              return setNotification(e);
+            }
+            if (e.properties.contains(CharacteristicProperties.INDICATE)) {
+              return setIndication(e);
+            }
+            return Future.value(true);
+          }),
+        )
+        .toList();
     final checkNotify = value.characteristic.properties
             .contains(CharacteristicProperties.NOTIFY) ||
         value.characteristic.properties
             .contains(CharacteristicProperties.INDICATE);
     if (checkNotify) {
-      await Future.forEach(listNotifications, (notify) => notify);
+      // final result = await Future.wait(listNotifications);
+      // if (result.any((e) => e == false)) {
+      //   return [];
+      // }
+      for (final notification in listNotifications) {
+        await Future<void>.delayed(const Duration(milliseconds: 350));
+        final r = await notification;
+        if (!r) {
+          return [];
+        }
+      }
       await Future<void>.delayed(const Duration(milliseconds: 450));
     }
     final isWrite = await writeCharacteristicNoResponse(value);
     if (!checkNotify) {
-      await Future.forEach(listNotifications, (notify) => notify);
+      for (final notification in listNotifications) {
+        await Future<void>.delayed(const Duration(milliseconds: 350));
+        final r = await notification;
+        if (!r) {
+          return [];
+        }
+      }
     }
     if (!isWrite) {
       listen.cancel();
